5a81a6096671d094075f5fa4667e8bfb69689201
Ilya Kreymer <ikreymer@users.noreply.github.com>
2024-11-16 14:34:54 -0800

    Update init.js to global-change.gov settings

--- init.js
+++ init.js
@@ -1,2 +1,2 @@
 // Fill in WACZ and origin here
-// init("https://example.com/path/to/my/file.wacz", "https://my-archived-site.example.com");
+init("https://replayweb.page?source=https%3A%2F%2Fapp.browsertrix.com%2Fapi%2Forgs%2F75f05fd9-ffe0-447d-be70-66d1e4bc4b6d%2Fcollections%2F29db8c5a-7e2c-400a-a239-d0da78a340f5%2Fpublic%2Freplay.json", "https://globalchange.gov");
