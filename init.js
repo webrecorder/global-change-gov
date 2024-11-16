2f8735b43bb99e51386947b350f945665fd07103
Ilya Kreymer <ikreymer@users.noreply.github.com>
2024-11-16 14:41:25 -0800

    Update init.js

--- init.js
+++ init.js
@@ -1,2 +1,2 @@
 // Fill in WACZ and origin here
-init("https://replayweb.page?source=https%3A%2F%2Fapp.browsertrix.com%2Fapi%2Forgs%2F75f05fd9-ffe0-447d-be70-66d1e4bc4b6d%2Fcollections%2F29db8c5a-7e2c-400a-a239-d0da78a340f5%2Fpublic%2Freplay.json", "https://globalchange.gov");
+init("https://app.browsertrix.com/api/orgs/75f05fd9-ffe0-447d-be70-66d1e4bc4b6d/collections/29db8c5a-7e2c-400a-a239-d0da78a340f5/public/replay.json", "https://globalchange.gov");
